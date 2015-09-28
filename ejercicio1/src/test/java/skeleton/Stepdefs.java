032ad78ff99690191bd57f40aff3a59259924bd3
Diego Tabares <dtabares@gmail.com>
2015-09-27 23:59:55 -0300

    Cambio las expresiones regulares para capturar la figura jugada

--- ejercicio1/src/test/java/skeleton/Stepdefs.java
+++ ejercicio1/src/test/java/skeleton/Stepdefs.java
@@ -29,16 +29,31 @@ public class Stepdefs {
     		
     }
     
-    @Given("^jugador1 juega PIEDRA$")
-    public void jugador1_juega_PIEDRA() {		
+    @Given("^jugador1 juega (PIEDRA|PAPEL|TIJERA)$")
+    public void jugador1_juega(String nombreFigura) {		
     	this.jugador1 = new Jugador("JUGADOR1");
-    	this.jugador1.elegirFigura(new Piedra());
+    	switch(nombreFigura){
+    	case "PIEDRA":	this.jugador1.elegirFigura(new Piedra());
+    					break;
+    	case "PAPEL":	this.jugador1.elegirFigura(new Papel());
+    					break;
+    	case "TIJERA":	this.jugador1.elegirFigura(new Tijera());
+						break;
+    	}
+    	
     }
 
-    @When("^jugador2 juega PIEDRA$")
-    public void jugador2_juega_PIEDRA() {
+    @When("^jugador2 juega (PIEDRA|PAPEL|TIJERA)$")
+    public void jugador2_juega(String nombreFigura) {
     	this.jugador2 = new Jugador("JUGADOR2");
-    	this.jugador2.elegirFigura(new Piedra());
+    	switch(nombreFigura){
+    	case "PIEDRA":	this.jugador2.elegirFigura(new Piedra());
+    					break;
+    	case "PAPEL":	this.jugador2.elegirFigura(new Papel());
+    					break;
+    	case "TIJERA":	this.jugador2.elegirFigura(new Tijera());
+						break;
+    	}
 
     }
 
